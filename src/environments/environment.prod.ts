6fb852c96af5dee68b0fc87aedf44393bf76aee5
erwin-io <erwinramirez220@gmail.com>
2025-06-03 17:57:55 +0800

    3rd commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,7 @@ export const environment = {
   oneSignalAppId: "09c8c0ed-5d53-42f9-8384-c66c217bd87e",
   apiBaseUrl: "https://shaj-flower-shop-api.vercel.app/api/v1",
   openRouteServiceAPIKey: "5b3ce3597851110001cf6248a5e16664c28b4743a047d680fc89228e",
+  maximLocationServiceURL: "https://client-sea.taximaxim.com/en-US/service",
   api: {
     auth: {
       login: "/auth/login/customer/",
@@ -21,10 +22,43 @@ export const environment = {
       getItems: "/cart/getItems/",
       create: "/cart/",
       update: "/cart/",
-      manageCoupon: "/cart/coupon"
+      manageCoupon: "/cart/coupon",
+      getActiveCoupon: "/cart/customerUserCoupon/",
     },
     systemConfig: {
       getAll: "/system-config/"
     },
+    geolocation: {
+      searchAddress: "/geolocation/search/"
+    },
+    order: {
+      getAdvanceSearch: "/order/my-orders/",
+      getByCode: "/order/",
+      create: "/order/",
+      updateStatus: "/order/status/",
+    },
+    delivery: {
+      calculate: "/delivery/calculate/"
+    },
+    category: {
+      getAdvanceSearch: "/category/page/",
+      getByCode: "/category/",
+    },
+    collection: {
+      getAdvanceSearch: "/collection/page/",
+      getByCode: "/collection/",
+    },
+    product: {
+      getAdvanceSearch: "/product/page/",
+      getSearchFilter: "/product/get-search-filter/",
+      getByCode: "/product/",
+    },
+    customerUserWishlist: {
+      getAdvanceSearch: "/customer-user-wish-list/page/",
+      getByCode: "/customer-user-wish-list/",
+      getBySKU: "/customer-user-wish-list/get-by-sku/",
+      create: "/customer-user-wish-list/",
+      delete: "/customer-user-wish-list/",
+    }
   }
 };
